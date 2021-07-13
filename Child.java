e8497b4e81db75e657550705d906f5da975581b0
dgotlieb <Dgotlieb8@gmail.com>
2021-07-13 20:27:45 +0300

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,17 @@
+public class Child extends Parent {
+    private int a = 200;
+
+    // prints a variable directly from base class
+    private void getAFromBase() {
+        System.out.println(super.a);
+    }
+
+    private void getAFromChild() {
+        System.out.println(a);
+    }
+
+    // call method directly from parent
+    private void callShow() {
+        show(); // no need for super
+    }
+}
---
+++ Parent.java
@@ -0,0 +1,7 @@
+public class Parent {
+    int a = 300;
+
+    void show(){
+        System.out.println(400);
+    }
+}
